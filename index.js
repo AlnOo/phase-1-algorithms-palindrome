bd5f7d5e276a4d7d1773d73aa691668b1dc093a2
Allan <github email address>
2022-06-22 14:19:21 +0300

    palidrome lab

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const len = word.length;
+  for(let i = 0; i < len/2; i++){
+    if(word[i] !== word[len -1 -i]){
+      return false;
+    }
+  }
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -20,6 +19,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("chicken"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("anna"));
 }
 
 module.exports = isPalindrome;
